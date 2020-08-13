3f83f9106d024c21cb45ad2e60d9e13f4aa364a1
Joshua M <josh@auxilia.io>
2020-08-13 18:30:47 +0300

    Updated Profiles & Logos

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -4,11 +4,10 @@ jQuery(document).ready(function() {
         e.preventDefault();
         History.pushState({}, "", this.pathname);
     });
-
-    var _gaq = [
-        ["_setAccount", "UA-65148661-1"],
-        ["_trackPageview"]
-    ];
+    // var _gaq = [
+    //     ["_setAccount", "UA-65148661-1"],
+    //     ["_trackPageview"]
+    // ];
     (function(d, t) {
         var g = d.createElement(t),
             s = d.getElementsByTagName(t)[0];
@@ -16,14 +15,13 @@ jQuery(document).ready(function() {
         g.src = ("https:" == location.protocol ? "//ssl" : "//www") + ".google-analytics.com/ga.js";
         s.parentNode.insertBefore(g, s)
     }(document, "script"));
-
     History.Adapter.bind(window, 'statechange', function() {
         var State = History.getState();
         $.get(State.url, function(data) {
             document.title = data.match(/<title>(.*?)<\/title>/)[1];
             $('.content').html($(data).find('.content'));
             $('html, body').animate({ scrollTop: 0 }, 'slow');
-            _gaq.push(['_trackPageview', State.url]);
+            // _gaq.push(['_trackPageview', State.url]);
         });
     });
      AOS.init({
